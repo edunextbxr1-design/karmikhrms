33c57c540c81dc033d92b980567afd5cb4836c74
edunextbxr1-design <edunext.bxr1@gmail.com>
2026-01-30 23:35:28 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'edunext-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
